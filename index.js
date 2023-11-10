c009c6f8af4e2810ff67f0d13ad599396b71f23c
jfaduna <flomeraduna@gmail.com>
2023-11-10 18:16:50 +1100

    modify: update console.log()

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const langs = require("langs")
 const input = process.argv[2]
 const langCode = franc(input)
 if (langCode === 'und') {
-    console.log("Sorry, Couldn't figure it out. Try different sample text.")
+    console.log("Language cannot be guessed. Try different sample text.")
 } else {
     const language = langs.where("3", langCode)
-    console.log(language.name)
+    console.log("Best Guess: " + language.name)
 }
